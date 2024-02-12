82c6a5de1587317eece6063f63c41604a8e8ff2a
zweroboy1 <fprognoz@gmail.com>
2024-02-12 12:52:17 +0200

    test: fix partial mocking test

--- src/05-partial-mocking/index.test.ts
+++ src/05-partial-mocking/index.test.ts
@@ -13,23 +13,27 @@ jest.mock('./index', () => {
 });
 
 describe('partial mocking', () => {
+  beforeEach(() => {
+    jest.spyOn(console, 'log').mockImplementation();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   afterAll(() => {
     jest.unmock('./index');
   });
 
   test('mockOne, mockTwo, mockThree should not log into console', () => {
-    const consoleSpy = jest.spyOn(console, 'log');
     mockOne();
     mockTwo();
     mockThree();
-    expect(consoleSpy).not.toHaveBeenCalled();
-    consoleSpy.mockRestore();
+    expect(console.log).not.toHaveBeenCalled();
   });
 
   test('unmockedFunction should log into console', () => {
-    const consoleSpy = jest.spyOn(console, 'log');
     unmockedFunction();
-    expect(consoleSpy).toHaveBeenCalled();
-    consoleSpy.mockRestore();
+    expect(console.log).toHaveBeenCalled();
   });
 });
